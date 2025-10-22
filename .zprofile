90c8c4af5395d1845ecb2e52736eb4abeabd4477
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-10-22 16:45:17 +0530

    Initial commit of DWM dotfiles and Suckless configs

---
+++ .zprofile
@@ -0,0 +1,6 @@
+export PATH=$HOME/.local/bin:$PATH
+
+
+if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]; then
+	exec startx
+fi
---
+++ dwmblocks/config.h
@@ -0,0 +1,13 @@
+static const Block blocks[] = {
+
+/*Icon*/        /*Command*/                   /*Update Interval*/     /*Update Signal*/
+  {"",  "music", 0.1,   11},
+  {"",  "volume",   1,     10},
+  {"",  "clock",    60,  1},
+  {"",  "internet", 5,     4},
+  {"",  "battery",  5,     4},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
