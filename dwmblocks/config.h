b66e945e1d835413f1a967c750e04f8531cfa7a2
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-10-23 12:11:41 +0530

    fixes

--- dwmblocks/config.h
+++ dwmblocks/config.h
@@ -1,11 +1,11 @@
 static const Block blocks[] = {
 
-/*Icon*/        /*Command*/                   /*Update Interval*/     /*Update Signal*/
-  {"",  "music", 0,   11},
-  {"",  "volume",   1,     10},
-  {"",  "clock",    60,  1},
-  {"",  "internet", 5,     4},
-  {"",  "battery",  5,     4},
+/*Icon*/      /*Command*/       /*Update Interval*/    /*Update Signal*/
+  {"",  "music",     0,            11},
+  {"",  "volume",    1,            10},
+  {"",  "clock",     10,           1},
+  {"",  "internet",  5,            4},
+  {"",  "battery",   5,            4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
